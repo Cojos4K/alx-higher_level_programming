00a284cddcc9c0e491bab82327bab65175dfb7db
“Cojos4K” <winsfev23@gmail.com>
2024-02-13 03:31:00 -0800

    1.3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 //Write a script that prints 3 lines using console.log
 let myVar = "C is fun";
+console.log(myVar);
 myVar = "Python is cool";
+console.log(myVar);
 myVar = "JavaScript is amazing";
 console.log(myVar);
